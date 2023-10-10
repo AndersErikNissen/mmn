0b922d2dabe68cb68d1374f76d07654ea511e1f0
Anders Erik Nissen <anderseriknissen@gmail.com>
2023-10-11 01:18:26 +0200

    Menu, navigation - done, added heading observer

--- assets/main.js
+++ assets/main.js
@@ -33,7 +33,29 @@ const classListMatch = (element, classList) => classList.some(clss => element.cl
         document.body.classList.remove('activeMenu');
       }
     } else {
+      if (!!event.target.closest('#Navigation')) return;
       if (!!event.target.closest('#Menu') == false && !inactiveMenu) document.body.classList.remove('activeMenu');
     }
   });
+})();
+
+// Heading observer
+(function() {
+  const observerCallback = (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        if (!entry.target.classList.contains('animateHeading')) {
+          entry.target.classList.add('animateHeading');
+        }
+      }
+    });
+  };
+
+  let observer = new IntersectionObserver(observerCallback, {
+    rootMargin: '0px',
+    threshold: 1.0,
+  });
+
+  let relevantHeadings = document.querySelectorAll('.heading');
+  relevantHeadings.forEach(heading => observer.observe(heading));
 })();
