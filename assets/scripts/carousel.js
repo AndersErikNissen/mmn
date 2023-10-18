f6823911bc4ffdcb81da56864003c1b0f8c7d40f
Anders Erik Nissen <anderseriknissen@gmail.com>
2023-10-19 00:58:12 +0200

    Work on slider timing

--- assets/scripts/carousel.js
+++ assets/scripts/carousel.js
@@ -1,14 +1,11 @@
 (function() {
   class Button {
-    constructor(node, index) {
+    constructor(node) {
       this.node = node;
-      this.polar = index % 2;
       this.init = function(self) {
         function signal(e) {
-          let direction = self.polar === 0 ? 'prev' : 'next';
-          self.node.dispatchEvent(new CustomEvent('carousel:' + direction, { bubbles: true, detail: { polar: direction === 'prev' ? -1 : 1 }}));
+          self.node.dispatchEvent(new CustomEvent('move:slider', { bubbles: true, detail: { index: self.node.classList.contains('prevSlide') ? -1 : 1 }}));
         }
-
         self.node.addEventListener('click', signal);
       }(this);
     }
@@ -19,16 +16,61 @@
     constructor(node) {
       this.node = node;
       this.slider = node.querySelector('.slider');
-      this.multiplier = 100;
+      this.multiplier = -100;
       this.index = 0;
       this.slides = node.querySelectorAll('.slide');
+      this.lastIndex = this.slides.length - 1;
 
       this.buttons = function() {
-        let allButtonNodes = node.querySelectorAll('.carouselButton');
+        let allButtonNodes = node.querySelectorAll('.prevSlide, .nextSlide');
         let allButtons = [];
-        allButtonNodes.forEach((button, index) => allButtons.push(new Button(button, index)));
+        allButtonNodes.forEach(button => allButtons.push(new Button(button)));
         return allButtons;
       }.bind(this)();
+
+    }
+    init() {
+      this.node.addEventListener('move:slider', this.slideEvent.bind(this));
+      this.boundrySlide(this.index);
+    };
+
+    boundrySlide(i) {
+      if (i === 0) {
+        this.slides[this.lastIndex].style.transform = 'translateX(-100%)';
+      }
+      
+      if (i === this.lastIndex) {
+        this.slides[0].style.transform = 'translateX(-' + (this.lastIndex * 100) + ')';
+      }
+    }
+
+    slideEvent(e) {
+      let nextIndex = this.index + e.detail.index;
+      let lastSlideTransform = this.lastIndex * this.multiplier;
+      
+      this.slider.style.transform = 'translateX(' + (this.multiplier * nextIndex) + '%)';
+      
+      console.warn("SlideEvent",nextIndex, this.multiplier, this.slider)
+      if (nextIndex === 0) this.slides[this.lastIndex].style.transform = 'translateX(-100%)';
+      if (nextIndex === this.lastIndex) this.slides[0].style.transform = 'translateX(-' + lastSlideTransform + ')';
+      
+      if (nextIndex === -1) {
+        setTimeout(() => {
+          this.node.classList.add('noTransition');
+          console.log("first>last", (this.lastIndex * (this.multiplier * -1)), (this.lastIndex * this.multiplier))
+          
+          nextIndex = this.lastIndex;
+
+          this.slides[this.lastIndex].style.transform = 'translateX(' + (this.lastIndex * (this.multiplier * -1)) + '%)';
+          this.slider.style.transform = 'translateX(' + (this.lastIndex * this.multiplier) + '%)';
+        },1000);
+        
+        setTimeout(() => {
+          this.node.classList.remove('noTransition');
+        }, 1010)
+      }
+      
+      this.index = nextIndex;
     }
 
     /*
@@ -75,14 +117,9 @@
       this.slider.transform = `translateX(${-index * this.multiplier})`;
       this.index = index;
     }
- 
-    moveSlider() {
-      this.node.addEventListener('carousel:prev', this.slideLeft.bind(this));
-      this.node.addEventListener('carousel:next', this.slideRight.bind(this));
-    };
   }
 
   const indexCarousel = new Carousel(document.querySelector("#CaseCarousel"));
-  indexCarousel.moveSlider();
+  indexCarousel.init();
   console.log(indexCarousel)
 })();
